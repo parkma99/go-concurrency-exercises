a1670b420418eaa08d9714a27faaf698a14e9368
parkma99 <park-ma@hotmail.com>
2024-02-29 17:15:39 +0800

    task 4

--- 4-graceful-sigint/main.go
+++ 4-graceful-sigint/main.go
@@ -13,9 +13,35 @@
 
 package main
 
+import (
+	"os"
+	"os/signal"
+)
+
 func main() {
 	// Create a process
 	proc := MockProcess{}
+
+	sig := make(chan os.Signal, 1)
+	defer close(sig)
+	signal.Notify(sig, os.Interrupt)
+
+	go func() {
+		<-sig
+		done := make(chan bool)
+
+		go func() {
+			proc.Stop()
+			done <- true
+		}()
+
+		select {
+		case <-done:
+			os.Exit(0)
+		case <-sig:
+			os.Exit(1)
+		}
+	}()
 
 	// Run the process (blocking)
 	proc.Run()
